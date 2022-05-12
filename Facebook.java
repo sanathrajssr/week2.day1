3f4b89ef8225a7bf8c40ff1a0451b91a01146e16
sanathrajssr <104908944+sanathrajssr@users.noreply.github.com>
2022-05-12 11:43:14 +0530

    Add files via upload

--- Facebook.java
+++ Facebook.java
@@ -22,13 +22,19 @@ public class Facebook {
 		driver.findElement(By.linkText("Create New Account")).click();
 		driver.findElement(By.name("firstname")).sendKeys("Sanath");
 		driver.findElement(By.name("lastname")).sendKeys("raj");
-		driver.findElement(By.name("reg_email_")).sendKeys("sanathrajssr2000@gmail.com");
-		driver.findElement(By.id("password step input")).sendKeys("SANATH@123");
+		driver.findElement(By.name("reg_email__")).sendKeys("sanathrajssr2000@gmail.com");
+		driver.findElement(By.id("password_step_input")).sendKeys("SANATH@123");
+		
+		
 		WebElement date = driver.findElement(By.id("day"));
-		Select day = new Select(date);
-		day.selectByVisibleText("09");
-		driver.findElement(By.name("Month")).sendKeys("MARCH");
-		driver.findElement(By.name("YEAR")).sendKeys("2000");
+		Select d1= new Select(date);
+		d1.selectByValue("9");
+		WebElement month = driver.findElement(By.id("month"));
+		Select d2 = new Select(month);
+		d2.selectByValue("3");
+		WebElement year = driver.findElement(By.id("year"));
+		Select d3 = new Select(year);
+		d3.selectByValue("2000");
 		
 
 	}
